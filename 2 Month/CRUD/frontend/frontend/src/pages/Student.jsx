d1fec386683a1ed360ff144a71646d82a3064f0e
akshaymore3008 <morea1997@gmail.com>
2025-06-19 20:02:49 +0530

    tabels

--- 2 Month/CRUD/frontend/frontend/src/pages/Student.jsx
+++ 2 Month/CRUD/frontend/frontend/src/pages/Student.jsx
@@ -67,7 +67,36 @@ const Student = () => {
 
 
 
-                    <div className="col-md-6"> Student List</div>
+                    <div className="col-md-6">  <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Username</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td colSpan={2}>Larry the Bird</td>
+                                <td>@twitter</td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                        );</div>
                 </div>
             </div>
             <h1>Student</h1>
