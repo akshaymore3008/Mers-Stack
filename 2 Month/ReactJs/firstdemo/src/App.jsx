25fd39f23c1ccdeaef358f18548b7a23631f5a6b
akshaymore3008 <morea1997@gmail.com>
2025-06-19 18:47:52 +0530

    card boostrap

--- 2 Month/ReactJs/firstdemo/src/App.jsx
+++ 2 Month/ReactJs/firstdemo/src/App.jsx
@@ -1,3 +1,6 @@
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from "react";
 
 function App() {
@@ -25,7 +28,97 @@ function App() {
       <button onClick={handleSubmit}>Submit</button>
 
       {showName && <h1>{name}</h1>}
+
+      <div className="card" >
+      
+          <div className="card-body">
+            <h5 className="card-title">Card title 1</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">Cras justo odio</li>
+            <li className="list-group-item">Dapibus ac facilisis in</li>
+            <li className="list-group-item">Vestibulum at eros</li>
+          </ul>
+          <div className="card-body">
+            <a href="#" className="card-link">Card link</a>
+            <a href="#" className="card-link">Another link</a>
+          </div>
+      </div>
+       <div className="card" >
+      
+          <div className="card-body">
+            <h5 className="card-title">Card title 2</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">Cras justo odio</li>
+            <li className="list-group-item">Dapibus ac facilisis in</li>
+            <li className="list-group-item">Vestibulum at eros</li>
+          </ul>
+          <div className="card-body">
+            <a href="#" className="card-link">Card link</a>
+            <a href="#" className="card-link">Another link</a>
+          </div>
+      </div>
     </div>
+  );
+   <div className="card" >
+      
+          <div className="card-body">
+            <h5 className="card-title">Card title 3</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">Cras justo odio</li>
+            <li className="list-group-item">Dapibus ac facilisis in</li>
+            <li className="list-group-item">Vestibulum at eros</li>
+          </ul>
+          <div className="card-body">
+            <a href="#" className="card-link">Card link</a>
+            <a href="#" className="card-link">Another link</a>
+          </div>
+      </div>
+    </div>
+  );
+   <div className="card" >
+      
+          <div className="card-body">
+            <h5 className="card-title">Card title 4</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">Cras justo odio</li>
+            <li className="list-group-item">Dapibus ac facilisis in</li>
+            <li className="list-group-item">Vestibulum at eros</li>
+          </ul>
+          <div className="card-body">
+            <a href="#" className="card-link">Card link</a>
+            <a href="#" className="card-link">Another link</a>
+          </div>
+      </div>
+    </div>
+  );
+   <div className="card" >
+      
+          <div className="card-body">
+            <h5 className="card-title">Card title 5</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">Cras justo odio</li>
+            <li className="list-group-item">Dapibus ac facilisis in</li>
+            <li className="list-group-item">Vestibulum at eros</li>
+          </ul>
+          <div className="card-body">
+            <a href="#" className="card-link">Card link</a>
+            <a href="#" className="card-link">Another link</a>
+          </div>
+      </div>
+    </div>
+  );
+    </div>
+    
   );
 }
 
