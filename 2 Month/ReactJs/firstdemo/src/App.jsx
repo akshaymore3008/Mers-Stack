2360fcd80629965688dd87faf7a3d341626aec2d
akshaymore3008 <morea1997@gmail.com>
2025-06-20 15:23:16 +0530

    card change

--- 2 Month/ReactJs/firstdemo/src/App.jsx
+++ 2 Month/ReactJs/firstdemo/src/App.jsx
@@ -1,13 +1,12 @@
 
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from "react";
+import Card from './Card';
 
 function App() {
-  const [name, setName] = useState(""); // 1 hook
-  const [showName, setShowName] = useState(false);
+  const [name, setName] = useState(); //Hooks
 
-  console.log(name);
+  const [showName, setShowName] = useState(false);
 
   const handleOnChange = (e) => {
     console.log(e.target.value);
@@ -18,108 +17,32 @@ function App() {
     setShowName(true);
   };
 
+  const [cardData, setCardData] = useState(
+    [
+      { title: "Title - 1" },
+      { title: "Title - 2" },
+      { title: "Title - 3" },
+      { title: "Title - 4" },
+    ]
+  );
+
   return (
-    <div>
+    <>
       <input
         name="name"
         placeholder="Enter Your Name"
         onChange={handleOnChange}
       />
-      <button onClick={handleSubmit}>Submit</button>
 
+      <button onClick={handleSubmit}>Submit</button>
       {showName && <h1>{name}</h1>}
 
-      <div className="card" >
-      
-          <div className="card-body">
-            <h5 className="card-title">Card title 1</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">Cras justo odio</li>
-            <li className="list-group-item">Dapibus ac facilisis in</li>
-            <li className="list-group-item">Vestibulum at eros</li>
-          </ul>
-          <div className="card-body">
-            <a href="#" className="card-link">Card link</a>
-            <a href="#" className="card-link">Another link</a>
-          </div>
-      </div>
-       <div className="card" >
-      
-          <div className="card-body">
-            <h5 className="card-title">Card title 2</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">Cras justo odio</li>
-            <li className="list-group-item">Dapibus ac facilisis in</li>
-            <li className="list-group-item">Vestibulum at eros</li>
-          </ul>
-          <div className="card-body">
-            <a href="#" className="card-link">Card link</a>
-            <a href="#" className="card-link">Another link</a>
-          </div>
-      </div>
-    </div>
-  );
-   <div className="card" >
-      
-          <div className="card-body">
-            <h5 className="card-title">Card title 3</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">Cras justo odio</li>
-            <li className="list-group-item">Dapibus ac facilisis in</li>
-            <li className="list-group-item">Vestibulum at eros</li>
-          </ul>
-          <div className="card-body">
-            <a href="#" className="card-link">Card link</a>
-            <a href="#" className="card-link">Another link</a>
-          </div>
-      </div>
-    </div>
-  );
-   <div className="card" >
-      
-          <div className="card-body">
-            <h5 className="card-title">Card title 4</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">Cras justo odio</li>
-            <li className="list-group-item">Dapibus ac facilisis in</li>
-            <li className="list-group-item">Vestibulum at eros</li>
-          </ul>
-          <div className="card-body">
-            <a href="#" className="card-link">Card link</a>
-            <a href="#" className="card-link">Another link</a>
-          </div>
-      </div>
-    </div>
-  );
-   <div className="card" >
-      
-          <div className="card-body">
-            <h5 className="card-title">Card title 5</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">Cras justo odio</li>
-            <li className="list-group-item">Dapibus ac facilisis in</li>
-            <li className="list-group-item">Vestibulum at eros</li>
-          </ul>
-          <div className="card-body">
-            <a href="#" className="card-link">Card link</a>
-            <a href="#" className="card-link">Another link</a>
-          </div>
-      </div>
-    </div>
-  );
-    </div>
-    
+      {cardData.map((each) => (
+        <Card title={each.title} />
+      ))}
+
+    </>
   );
 }
 
-export default App;
+export default App
