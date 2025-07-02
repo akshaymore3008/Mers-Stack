8905823bb5172023c1adb8e8f580ee6e1ac65d75
akshaymore3008 <morea1997@gamil.com>
2025-07-02 18:26:52 +0530

    Done

--- 2 Month/CRUD/frontend/frontend/src/App.jsx
+++ 2 Month/CRUD/frontend/frontend/src/App.jsx
@@ -1,18 +1,13 @@
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Student from "./pages/Student"
-
+import './app.css'
+import Student from "./pages/Student";
 
 function App() {
-
   return (
     <>
-      <h1>Akshay more</h1>
-      <Student />
-
+      <Student/>
     </>
-
-  )
+  );
 }
 
-export default App
+export default App;
--- 2 Month/CRUD/frontend/frontend/src/pages/Student.jsx
+++ 2 Month/CRUD/frontend/frontend/src/pages/Student.jsx
@@ -1,109 +1,110 @@
 
-
-import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
+import Table from 'react-bootstrap/Table'; 
 
 const Student = () => {
     return (
         <div>
+            <h1 className="text-center my-4">Student</h1> 
             <div className="container">
                 <div className="row">
-                    <div className="col-md-6">  <Form>
-                        <Row className="mb-3">
-                            <Form.Group as={Col} controlId="formGridEmail">
-                                <Form.Label>Email</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email" />
+                    <div className="col-md-6">
+                        <Form>
+                            <Row className="mb-3">
+                                <Form.Group as={Col} controlId="formGridEmail">
+                                    <Form.Label>Email</Form.Label>
+                                    <Form.Control type="email" placeholder="Enter email" />
+                                </Form.Group>
+
+                                <Form.Group as={Col} controlId="formGridPassword">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control type="password" placeholder="Password" />
+                                </Form.Group>
+                            </Row>
+
+                            <Form.Group className="mb-3" controlId="formGridAddress1">
+                                <Form.Label>Address</Form.Label>
+                                <Form.Control placeholder="1234 Main St" />
                             </Form.Group>
 
-                            <Form.Group as={Col} controlId="formGridPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" />
-                            </Form.Group>
-                        </Row>
-
-                        <Form.Group className="mb-3" controlId="formGridAddress1">
-                            <Form.Label>Address</Form.Label>
-                            <Form.Control placeholder="1234 Main St" />
-                        </Form.Group>
-
-                        <Form.Group className="mb-3" controlId="formGridAddress2">
-                            <Form.Label>Address 2</Form.Label>
-                            <Form.Control placeholder="Apartment, studio, or floor" />
-                        </Form.Group>
-
-                        <Row className="mb-3">
-                            <Form.Group as={Col} controlId="formGridCity">
-                                <Form.Label>City</Form.Label>
-                                <Form.Control />
+                            <Form.Group className="mb-3" controlId="formGridAddress2">
+                                <Form.Label>Address 2</Form.Label>
+                                <Form.Control placeholder="Apartment, studio, or floor" />
                             </Form.Group>
 
-                            <Form.Group as={Col} controlId="formGridState">
-                                <Form.Label>State</Form.Label>
-                                <Form.Select defaultValue="Choose...">
-                                    <option>Choose...</option>
-                                    <option>...</option>
-                                </Form.Select>
+                            <Row className="mb-3">
+                                <Form.Group as={Col} controlId="formGridCity">
+                                    <Form.Label>City</Form.Label>
+                                    <Form.Control />
+                                </Form.Group>
+
+                                <Form.Group as={Col} controlId="formGridState">
+                                    <Form.Label>State</Form.Label>
+                                    <Form.Select defaultValue="Choose...">
+                                        <option>Choose...</option>
+                                        <option>New York</option>
+                                        <option>California</option>
+                                        <option>Texas</option>
+                                    </Form.Select>
+                                </Form.Group>
+
+                                <Form.Group as={Col} controlId="formGridZip">
+                                    <Form.Label>Zip</Form.Label>
+                                    <Form.Control />
+                                </Form.Group>
+                            </Row>
+
+                            <Form.Group className="mb-3" id="formGridCheckbox">
+                                <Form.Check type="checkbox" label="Check me out" />
                             </Form.Group>
 
-                            <Form.Group as={Col} controlId="formGridZip">
-                                <Form.Label>Zip</Form.Label>
-                                <Form.Control />
-                            </Form.Group>
-                        </Row>
-
-                        <Form.Group className="mb-3" id="formGridCheckbox">
-                            <Form.Check type="checkbox" label="Check me out" />
-                        </Form.Group>
-
-                        <Button variant="primary" type="submit">
-                            Submit
-                        </Button>
-                    </Form>
+                            <Button variant="primary" type="submit">
+                                Submit
+                            </Button>
+                        </Form>
                     </div>
 
-
-
-
-                    <div className="col-md-6">  <Table striped bordered hover>
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>First Name</th>
-                                <th>Last Name</th>
-                                <th>Username</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                            </tr>
-                            <tr>
-                                <td>2</td>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
-                            </tr>
-                            <tr>
-                                <td>3</td>
-                                <td colSpan={2}>Larry the Bird</td>
-                                <td>@twitter</td>
-                            </tr>
-                        </tbody>
-                    </Table>
-                        );</div>
+                    <div className="col-md-6">
+                        <Table striped bordered hover>
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>First Name</th>
+                                    <th>Last Name</th>
+                                    <th>Username</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>Jacob</td>
+                                    <td>Thornton</td>
+                                    <td>@fat</td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td colSpan={2}>Larry the Bird</td>
+                                    <td>@twitter</td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </div>
                 </div>
             </div>
-            <h1>Student</h1>
-
         </div>
+    );
+};
 
-    )
+export default Student;
 
-}
-export default Student
